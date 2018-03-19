411f0d7ce144b60c3a66858d0e91efcb1b695812
Xuefeng Ding <xuefeng.ding.physics@gmail.com>
2018-03-19 20:36:17 +0100

    now scale of test is tunable

--- timer.cc
+++ timer.cc
@@ -5,34 +5,35 @@
 void test(std::function<void()> t,int N) {
   clock_t startCPU, stopCPU;
   timeval startTime, stopTime, totalTime;
-  tms startProc, stopProc;
+//  tms startProc, stopProc;
   gettimeofday(&startTime, NULL);
-  startCPU = times(&startProc);
+//  startCPU = times(&startProc);
 
   for(int i = 0 ;i<N;++i) {
     t();
   }
 
-  stopCPU = times(&stopProc);
+//  stopCPU = times(&stopProc);
   gettimeofday(&stopTime, NULL);
-  double myCPU = stopCPU - startCPU;
-  double totalCPU = myCPU;
+//  double myCPU = stopCPU - startCPU;
+//  double totalCPU = myCPU;
   timersub(&stopTime, &startTime, &totalTime);
   std::cout << "Wallclock time  : " << totalTime.tv_sec + totalTime.tv_usec/1000000.0 << " seconds."<<std::endl;
-  std::cout << "CPU time: " << (myCPU / CLOCKS_PER_SEC) << std::endl;
-  std::cout << "Total CPU time: " << (totalCPU / CLOCKS_PER_SEC) << std::endl;
-  myCPU = stopProc.tms_utime - startProc.tms_utime;
-  std::cout << "Processor time: " << (myCPU / CLOCKS_PER_SEC) << std::endl;
+//  std::cout << "CPU time: " << (myCPU / CLOCKS_PER_SEC) << std::endl;
+//  std::cout << "Total CPU time: " << (totalCPU / CLOCKS_PER_SEC) << std::endl;
+//  myCPU = stopProc.tms_utime - startProc.tms_utime;
+//  std::cout << "Processor time: " << (myCPU / CLOCKS_PER_SEC) << std::endl;
 }
 void logLL();
 void RPF();
 void MVLL();
 int main(int argc,char *argv[]) {
-  if(argc==1) std::cout<<argv[0]<<std::endl;
+  if(argc!=2) { std::cout<<"usage: "<<argv[0]<<" <N> where N is the scale of the test."<<std::endl; return 0; }
+  int N = atoi(argv[1]);
   test(logLL,10);
-  test(logLL,10000);
-  test(RPF,10000);
-  test(MVLL,10000);
+  test(logLL,N);
+  test(RPF,N);
+  test(MVLL,N);
   return 0;
 }
 

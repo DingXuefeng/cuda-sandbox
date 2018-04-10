95c234087e5f2686b7abbf7df1953b4310b487d9
Xuefeng Ding <xuefeng.ding.physics@gmail.com>
2018-04-11 00:46:18 +0200

    more test

--- root/stdmap_split/savemap.C
+++ root/stdmap_split/savemap.C
@@ -16,27 +16,29 @@ struct Element {
 struct info {
   std::map<std::string,Element*> elements;
   std::map<std::string,int> simple_elements;
-  std::vector<Element*> e;
+  std::vector<const Element*> e;
 };
 void savemap() {
 //  TFile *file = TFile::Open("save.root","RECREATE");
   TTree *tree = new TTree("t","t");
-  info *obj = new info();
-
+  info *obj = new info {{{"hi", new Element{1,1,1}},{"h",new Element { 3,3,3}}} ,{{"hi",1},{"m",2}} , { new Element { 1,2,3}, new Element { 2,3,3} }};
   tree->Branch("t","info",&obj,84000,9999);
+  tree->Fill();
+
   (obj->elements)["hi"] = new Element { 1,2,3 };
   (obj->elements)["oh"] = new  Element { 2,3,1 };
   std::vector<Element*> e;
   e.push_back( obj->elements["hi"] );
-  obj->e = e;
+//  obj->e = e;
   obj->simple_elements["hh"] = 3;
   obj->simple_elements["hd"] = 4;
-  tree->Fill();
   (obj->elements)["h"] = new Element { 9,2,3 };
   (obj->elements)["o"] =new  Element { 12,3,1 };
   obj->simple_elements["x"] = 5;
   tree->Fill();
   tree->Print();
+  tree->Show(0);
+  tree->Show(1);
 TClass *cl = TClass::GetClass("info");
   TStreamerInfo* sinfo = tree->BuildStreamerInfo(cl, obj, false);
 sinfo->ls();

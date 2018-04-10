b5269c779b1bc357a9733fe4060cf13bf1d7aa6d
Xuefeng Ding <xuefeng.ding.physics@gmail.com>
2018-04-10 23:37:28 +0200

    root cannot split map

---
+++ root/stdmap_split/savemap.C
@@ -0,0 +1,44 @@
+#include <map>
+#include <string>
+#include <TFile.h>
+#include <TTree.h>
+#include "TStreamerInfo.h"
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma C++ class std::map<std::string,Element*>+;
+#pragma C++ class std::map<std::string,int>+;
+struct Element {
+  double x;
+  double y;
+  double z;
+};
+struct info {
+  std::map<std::string,Element*> elements;
+  std::map<std::string,int> simple_elements;
+  std::vector<Element*> e;
+};
+void savemap() {
+//  TFile *file = TFile::Open("save.root","RECREATE");
+  TTree *tree = new TTree("t","t");
+  info *obj = new info();
+
+  tree->Branch("t","info",&obj,84000,9999);
+  (obj->elements)["hi"] = new Element { 1,2,3 };
+  (obj->elements)["oh"] = new  Element { 2,3,1 };
+  std::vector<Element*> e;
+  e.push_back( obj->elements["hi"] );
+  obj->e = e;
+  obj->simple_elements["hh"] = 3;
+  obj->simple_elements["hd"] = 4;
+  tree->Fill();
+  (obj->elements)["h"] = new Element { 9,2,3 };
+  (obj->elements)["o"] =new  Element { 12,3,1 };
+  obj->simple_elements["x"] = 5;
+  tree->Fill();
+  tree->Print();
+TClass *cl = TClass::GetClass("info");
+  TStreamerInfo* sinfo = tree->BuildStreamerInfo(cl, obj, false);
+sinfo->ls();
+//  file->Close();
+}
